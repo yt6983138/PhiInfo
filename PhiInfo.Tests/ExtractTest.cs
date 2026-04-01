1e8808165387769f531f07ff42386f7b61a98221
yt6983138 <yt6983138@gmail.com>
2026-04-01 21:38:52 +0800

    Add tests (UNTESTED)

---
+++ PhiInfo.Tests/ExtractTest.cs
@@ -0,0 +1,20 @@
+﻿using PhiInfo.Core;
+using PhiInfo.Core.Models.Raw;
+
+namespace PhiInfo.Tests;
+
+[TestClass]
+public sealed class ExtractTest
+{
+	[TestMethod]
+	public void TestExtraction()
+	{
+		using PhigrosRawAssetExtractor rawExtractor = PhigrosRawAssetExtractor.FromApk(
+			File.OpenRead("./TestData/base.apk"),
+			File.OpenRead("./TestData/obb.obb"),
+			File.OpenRead("./TestData/classdata.tpk"));
+
+		PhigrosExtractedDataCollection info = rawExtractor.ExtractAll();
+		Console.WriteLine(info);
+	}
+}
