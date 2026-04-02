1ad5967dc31c3f0f815d6de87c7a850b71b8c151
yt6983138 <yt6983138@gmail.com>
2026-04-02 21:30:16 +0800

    Remove unsupported projects

--- PhiInfo.HttpServer/Type.cs
+++
@@ -1,8 +0,0 @@
-﻿#pragma warning disable IDE1006
-#pragma warning disable IDE0130
-
-namespace global.PhiInfo.HttpServer.Type;
-
-public record AppInfo(string version, string type);
-
-public record ServerInfo(string version, string platform, AppInfo app);
