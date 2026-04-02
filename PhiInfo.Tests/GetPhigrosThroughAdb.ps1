b2996317cc6ec545ac5f9ec532aa9186604487e2
yt6983138 <yt6983138@gmail.com>
2026-04-02 19:41:56 +0800

    Add auto extraction script from adb

---
+++ PhiInfo.Tests/GetPhigrosThroughAdb.ps1
@@ -0,0 +1,9 @@
+﻿$PhigrosPath = (adb shell pm path com.PigeonGames.Phigros).Substring(8)
+$ObbPath = "/storage/emulated/0/Android/obb/com.PigeonGames.Phigros/" + (adb shell ls -1 /storage/emulated/0/Android/obb/com.PigeonGames.Phigros).Split("`n")[0]
+echo $PhigrosPath
+echo $ObbPath
+
+adb pull $PhigrosPath .\TestData\base.apk
+adb pull $ObbPath .\TestData\obb.obb
+
+#New-Item -Path ".\TestData\obb.obb" -ItemType SymbolicLink -Value ".\TestData\_obb.obb" -Force
