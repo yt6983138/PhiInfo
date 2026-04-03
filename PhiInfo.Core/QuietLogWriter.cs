afe010dc370a47820e6daa6af7cbcc78e81e31e0
yt6983138 <yt6983138@gmail.com>
2026-04-03 13:39:38 +0800

    Adding different loggers for cpp2il

---
+++ PhiInfo.Core/QuietLogWriter.cs
@@ -0,0 +1,10 @@
+﻿using LibCpp2IL.Logging;
+
+namespace PhiInfo.Core;
+public class QuietLogWriter : LogWriter
+{
+	public override void Error(string message) { }
+	public override void Info(string message) { }
+	public override void Verbose(string message) { }
+	public override void Warn(string message) { }
+}
---
+++ PhiInfo.Core/StreamLogWriter.cs
@@ -0,0 +1,40 @@
+﻿using LibCpp2IL.Logging;
+using System.Text;
+
+namespace PhiInfo.Core;
+public class StreamLogWriter : LogWriter, IDisposable
+{
+	public Stream Stream { get; set; }
+	public Encoding Encoding { get; set; }
+
+	public StreamLogWriter(Stream stream, Encoding encoding)
+	{
+		this.Stream = stream;
+		this.Encoding = encoding;
+	}
+	~StreamLogWriter()
+	{
+		this.Dispose();
+	}
+
+	private void Write(string message)
+	{
+		byte[] bytes = this.Encoding.GetBytes(message);
+		this.Stream.Write(bytes);
+	}
+
+	public override void Info(string message)
+		=> this.Write(message);
+	public override void Warn(string message)
+		=> this.Write(message);
+	public override void Error(string message)
+		=> this.Write(message);
+	public override void Verbose(string message)
+		=> this.Write(message);
+
+	public void Dispose()
+	{
+		GC.SuppressFinalize(this);
+		this.Stream.Dispose();
+	}
+}
